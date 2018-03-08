ed52d901322ef8ff14be6ec3238042fcce6d8351
flavio <fdanesse@gmail.com>
2018-03-08 15:46:06 -0300

    navbar y footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
+  user: any;
+
+  constructor () {}
+
+  actualizarCopyRigth() {
+    const footer = document.body.getElementsByClassName('navbar-text')[0];
+    const d = new Date();
+    footer.innerHTML = footer.innerHTML.replace('2018', d.getFullYear() + '');
+  }
+
+  ngOnInit() {
+    this.actualizarCopyRigth();
+  }
 }
